e7831510cc8cd82e8ee28d0c9e8b8f1366cabf92
Fernanda de Azevedo Fiuza <88252251+Fenandax@users.noreply.github.com>
2022-05-17 13:36:08 -0300

    index adicionado

--- src/main/java/com/desafiowl/DesafioWL/controller/ColaboradorController.java
+++ src/main/java/com/desafiowl/DesafioWL/controller/ColaboradorController.java
@@ -2,6 +2,7 @@ package com.desafiowl.DesafioWL.controller;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,23 +16,27 @@ import com.desafiowl.DesafioWL.repositories.ColaboradorRepository;
 @Controller
 public class ColaboradorController {
 
+	@Autowired
 	private ColaboradorRepository repo;
 
 	public ColaboradorController(ColaboradorRepository repo) {
 		this.repo = repo;
 	}
 
+	//mapeamento da URL
 	@GetMapping("/colaboradores")
 	public String colaboradores(Model model) {
 		model.addAttribute("listaColaboradores", repo.findAll());
 		return "colaboradores/index";
 	}
 
+	//cadastro de colaborador
 	@GetMapping("/colaboradores/cadastrar")
 	public String cadastrarColaborador(@ModelAttribute("colaborador") Colaborador colaborador) {
 		return "colaboradores/form";
 	}
 
+	//encontrar por ID
 	@GetMapping("colaboradores/{id}")
 	public String alterarColaborador(@PathVariable("id") long id, Model model) {
 		Optional<Colaborador> colaboradorOpt = repo.findById(id);
@@ -43,12 +48,14 @@ public class ColaboradorController {
 		return "colaboradores/form";
 	}
 
+	//salvar novo colaborador
 	@PostMapping("/colaboradores/salvar")
 	public String salvarColaborador(@ModelAttribute("colaborador") Colaborador colaborador) {
 		repo.save(colaborador);
 		return "colaboradores/form";
 	}
 
+	//remover colaborador
 	@GetMapping("/colaboradores/remover/{id}")
 	public String removerColaborador(@PathVariable("id") long id) {
 		Optional<Colaborador> colaboradorOpt = repo.findById(id);
