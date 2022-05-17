d823cb87885c4d2a7730637985240f5262efbc73
Fernanda de Azevedo Fiuza <88252251+Fenandax@users.noreply.github.com>
2022-05-17 14:13:04 -0300

    Insertion

--- src/main/java/com/desafiowl/DesafioWL/Insertion.java
+++ src/main/java/com/desafiowl/DesafioWL/Insertion.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.desafiowl.DesafioWL.entities.Colaborador;
+import com.desafiowl.DesafioWL.entities.Lanche;
 import com.desafiowl.DesafioWL.repositories.ColaboradorRepository;
+import com.desafiowl.DesafioWL.repositories.LancheRepository;
 
 @Component
 @Transactional
@@ -14,11 +17,32 @@ public class Insertion implements CommandLineRunner {
 
 	@Autowired
 	private ColaboradorRepository colaboradorRepository;
-	
+
+	@Autowired
+	private LancheRepository lancheRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		Colaborador c1 = new Colaborador(null, "Fernanda", "4545666687");
+		Colaborador c2 = new Colaborador(null, "Nicolas", "56218964");
+		Colaborador c3 = new Colaborador(null, "João", "645068823");
+
+		colaboradorRepository.save(c1);
+		colaboradorRepository.save(c2);
+		colaboradorRepository.save(c3);
+
+		Lanche l1 = new Lanche(null, "Sanduíche", c1);
+		Lanche l2 = new Lanche(null, "Café", c1);
+		Lanche l3 = new Lanche(null, "Bolo", c1);
+		Lanche l4 = new Lanche(null, "Torrada", c2);
+		Lanche l5 = new Lanche(null, "Chá", c3);
+
+		lancheRepository.save(l1);
+		lancheRepository.save(l2);
+		lancheRepository.save(l3);
+		lancheRepository.save(l4);
+		lancheRepository.save(l5);
 	}
 
-	
 }
